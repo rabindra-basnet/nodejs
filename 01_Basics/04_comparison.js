fae7165fb9d8440fc5848a34290dd16dcb95de4b
Rabindra Basnet <120715649+Rabindra-Basnet@users.noreply.github.com>
2024-09-03 12:50:52 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,7 @@
+// omit these types of comparison in code
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined >= 0);
