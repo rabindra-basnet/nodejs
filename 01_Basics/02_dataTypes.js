12771fd7f96c504fd365940c21f26a619663b4d5
Rabindra Basnet <120715649+Rabindra-Basnet@users.noreply.github.com>
2024-09-03 03:20:12 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // use for treating the newer version of javascript code
+
+//alert(3 +3) // only for browser modes
+
+console.log(3
+    +
+    3); //maiintain code readability
+
+console.log("Rai");
+
+let name = "Rabindra"
+let age = 22
+let isLoggin = false
+let state;
+
+//number -> 2 to power 53
+//bigint
+//string -> ""
+//boolean -> true/false
+//null -> standalone value
+//undefined -> unassigned
+//symbol -> unique
