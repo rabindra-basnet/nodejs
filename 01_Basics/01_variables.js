71e64cd7d86f1c0fdf72a12dc24c5a9aa56a004c
Rabindra Basnet <120715649+Rabindra-Basnet@users.noreply.github.com>
2024-09-03 03:03:42 +0000

    Variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 123348
+let accountEmail = "Rabindra@google.com"
+var accountPassword = "123445"
+accountCity = "Kathmandu"
+let accountState;
+
+
+// accountId = 14241
+
+accountEmail ="rabi@hc.com"
+accountPassword = "142455"
+accountCity = "Lahan"
+
+console.log(accountId);
+
+/*
+prefer not using var
+due to issue generated on block scope and functional scope
+
+*/
+
+console.table([accountId,accountEmail,accountPassword, accountCity, accountState])
