c86f44f725efc68abe5abb6cbf4961b7e332182c
Rabindra Basnet <120715649+Rabindra-Basnet@users.noreply.github.com>
2024-09-03 03:36:13 +0000

    conversiondatatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = "83"
+// let score = "83abc"
+// let score = null
+// let score = undefined
+let score = true
+
+console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+"83" -> 33
+"83abc" -> NaN
+true ->1/false ->0
+*/
+
+// let isLoggedIn =1
+let isLoggedIn ="rabi"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 -> true / 0 -> false
+// " " =>false
+// "rabi" -> true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
