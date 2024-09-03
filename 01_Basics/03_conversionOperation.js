e534aa29428eca1654364ef25f96ae6a30008251
Rabindra Basnet <120715649+Rabindra-Basnet@users.noreply.github.com>
2024-09-03 12:50:13 +0000

    conversonoperator

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -4,12 +4,12 @@
 // let score = undefined
 let score = true
 
-console.log(typeof score);
+// console.log(typeof score);
 // console.log(typeof (score));
 
 let valueInNumber = Number(score);
 // console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 /*
 "83" -> 33
@@ -20,7 +20,7 @@ true ->1/false ->0
 // let isLoggedIn =1
 let isLoggedIn ="rabi"
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 
 // 1 -> true / 0 -> false
 // " " =>false
@@ -30,6 +30,47 @@ console.log(booleanisLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********** Operations ************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2%4);
+// console.log(2**4);
+
+let str1 = "Hello"
+let str2 = " Rabindra"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" +  2);
+// console.log(1 +  "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((1 + 3) * 5 % 4);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1= num2= num3 = 4 + 2
+
+let gameCounter = 100;
+++gameCounter
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
+
+
+
 
