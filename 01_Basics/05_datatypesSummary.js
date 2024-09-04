aa5ed64547e63bb23dfc27b87317cd2f76ffce96
Rabindra Basnet <120715649+Rabindra-Basnet@users.noreply.github.com>
2024-09-04 12:04:37 +0000

    deepcopy and shallowcopy

--- 01_Basics/05_datatypesSummary.js
+++ 01_Basics/05_datatypesSummary.js
@@ -65,3 +65,37 @@ userTwo.email = "hitesh@gmail.cm"
 
 console.log(userOne.email);
 console.log(userTwo.email);
+
+
+// important discussion about shallow copy and deep copy
+
+/*
+Shallow copy: creates an new object or element and  doesnot creates copies of elements but references the nested elements to new copy same to previous elements
+so, the nested elements have original references and does changes on original if we make changes on shallow copy element;
+*/
+// Original array with nested objects
+const originalArray = ["Charlie",{ name: "Alice" }, { name: "Bob" }];
+
+// Shallow copy using the slice method
+const shallowCopy = originalArray.slice();
+console.log(shallowCopy)
+
+// Modifying the shallow copy
+shallowCopy[1].name = "Rabindra";
+
+console.log(originalArray[1].name); // Output: "Rabindra"
+console.log(originalArray[1]); // Output: {name: "Rabindra"}
+
+/* 
+Deep Copy: means having infependent memory and copies all to the new memory and become unique in terms of manipulation of old array
+*/
+// Original array with nested objects
+const originalArray1 = [{ name: "Alice" }, { name: "Bob" }];
+
+// Deep copy using JSON.stringify and JSON.parse
+const deepCopy = JSON.parse(JSON.stringify(originalArray1));
+
+// Modifying the deep copy
+deepCopy[0].name = "Charlie";
+console.log(deepCopy); // Output: "Alice"
+console.log(originalArray1); // Output: "Alice"
